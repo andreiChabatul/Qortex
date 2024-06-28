49fdb41b5685a87eed05526409b00c7b0081aa78
Andrei Chabatul <andreichabatul@gmail.com>
2024-06-28 15:42:56 +0300

    feat: add page artist

---
+++ app/front/src/components/ArtistItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <q-card class="artist-card">
+        <img :src="`${avatar}.webp`" style="height: 150px; object-fit: contain;">
+
+        <q-card-section>
+            <div class="text-h6">{{ name }}</div>
+            <div class="text-subtitle2">{{ kind }}</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+            {{ desc }}
+        </q-card-section>
+    </q-card>
+</template>
+
+<script setup lang="ts">
+import { IArtistProps } from 'src/types';
+
+defineOptions({
+    name: 'ArtistItem'
+});
+
+withDefaults(defineProps<IArtistProps>(), {
+    id: '',
+    name: '',
+    avatar: '',
+    kind: '',
+    desc: '',
+});
+
+</script>
+
+<style lang="sass" scoped>
+.artist-card
+  width: 200px
+</style>
---
+++ app/front/src/pages/ArtistList.vue
@@ -0,0 +1,32 @@
+<template>
+    <h3 class="q-pl-xs text-weight-medium">Artist List</h3>
+
+    <div class="fit row wrap justify-center items-start content-start artist-container q-pa-xs">
+
+        <q-circular-progress v-if="store.loading" indeterminate size="90px" :thickness="0.2" color="blue"
+            track-color="transparent" />
+
+        <ArtistItem v-else v-for="artist in store.artists" :key="artist.id" :name="artist.name" :desc="artist.desc"
+            :kind="artist.kind" :avatar="artist.avatar">
+        </ArtistItem>
+        
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import ArtistItem from 'components/ArtistItem.vue';
+import { useCounterStore } from 'src/stores/app';
+const store = useCounterStore();
+store.loadingArtists();
+
+defineOptions({
+    name: 'ArtistList'
+});
+
+</script>
+
+<style lang="sass" scoped>
+.artist-container
+  gap: 20px
+</style>
